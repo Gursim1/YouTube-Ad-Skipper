4400881cf5b76f4050873b042ba9d4196e394dfa
Gursimranjit Singh <gursimranjitsingh44@gmail.com>
2023-03-17 19:57:10 +0100

    YouTube Ad Skipper

---
+++ skip_adds.py
@@ -0,0 +1,18 @@
+import pyautogui
+
+pyautogui.FAILSAFE = True
+
+
+def skip_ad():
+    print("Ad skipper is running...")
+    print("To stop the ad skipper, you need to shut the program or press Ctrl+C in the terminal.")
+    while True:
+        video_ad = pyautogui.locateCenterOnScreen("play.png") # type: ignore
+
+        if video_ad is not None:
+            break
+
+    if video_ad is not None:
+        pyautogui.click(video_ad)
+
+    skip_ad()
